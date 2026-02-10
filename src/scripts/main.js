c5abb08eeb911ad65c1ae4c9594a9a7b05081d2a
Ivanov Ihor <i.ivanovvv21122006@gmail.com>
2026-02-10 21:46:46 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
 // write code here
+const tree = document.querySelector('.tree');
+const items = document.querySelectorAll('li');
+
+items.forEach((element) => {
+  const span = document.createElement('SPAN');
+  const text = element.firstChild;
+
+  element.insertBefore(span, text);
+
+  span.appendChild(text);
+});
+
+tree.addEventListener('click', (e) => {
+  if (e.target.tagName !== 'SPAN') {
+    return;
+  }
+
+  const parentLi = e.target.closest('li');
+  const nextLi = parentLi.querySelector('ul');
+
+  if (!nextLi) {
+    return;
+  }
+  nextLi.hidden = !nextLi.hidden;
+});
